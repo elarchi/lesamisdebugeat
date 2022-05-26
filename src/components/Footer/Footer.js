b50d396f7924b7f667cc3e7b750613ae344b7d96
elarchi <emilie.leury@gmail.com>
2022-05-26 13:01:12 +0100

    home css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,14 @@ import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div className="footer__div container">
+    <div className="footer__div ">
+      <hr className="container" />
+      <br />
       <img
         src="https://res.cloudinary.com/dn7zdnm89/image/upload/v1652461008/lesAmisDuPaysDeBugeat/logo_bnfdvs.png"
         alt="logo-association"
       />
+
       <p>Website créé par Emilie Leury, 2022</p>
     </div>
   );
