33de22a07a3cf78aeef00a0f8179597992e002e5
elarchi <emilie.leury@gmail.com>
2022-05-26 11:33:50 +0100

    netlify :rocket:

--- src/components/Last/Last.js
+++ src/components/Last/Last.js
@@ -8,21 +8,21 @@ const Last = ({ title, events, span }) => {
       <div className="cards__div">
         <div className="card__div">
           <hr />
-          <img src={events.left.url} alt="even-picture" />
+          <img src={events.left.url} alt="even-left" />
           <h2>{events.left.title}</h2>
           <p>{events.left.text}</p>
         </div>
 
         <div className="card__div">
           <hr />
-          <img src={events.middle.url} alt="even-picture" />
+          <img src={events.middle.url} alt="even-middle" />
           <h2>{events.middle.title}</h2>
           <p>{events.middle.text}</p>
         </div>
 
         <div className="card__div">
           <hr />
-          <img src={events.right.url} alt="even-picture" />
+          <img src={events.right.url} alt="even-right" />
           <h2>{events.right.title}</h2>
           <p>{events.right.text}</p>
         </div>
